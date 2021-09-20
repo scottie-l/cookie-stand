c2b3ada1e75b4850e0ac3ee74148109fbab9252d
Scott Lease <scottie_lease@hotmail.com>
2021-09-20 16:09:20 -0700

    added other stores to sales.html

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 // leave global for all to reference later on.
-let hours= ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm']
-let seattleList= document.getElementById("Seattle")
+let hours= ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'] //Array for hours
+let seattleList= document.getElementById("Seattle") // Id to call out specific place in HTML page of where to plug in.
+let tokyoList= document.getElementById("Tokyo")
+let dubaiList= document.getElementById("Dubai")
+let parisList= document.getElementById("Paris")
+let limaList= document.getElementById("Lima")
 
 
 let Seattle = {
@@ -11,23 +15,130 @@ let Seattle = {
     randoNumber: function () {  // object function for random number generator
         return Math.floor(Math.random() * (this.maxCust - this.minCust +1)) + this.minCust;
     },
-    cookiesPurchased: function() {  // function for getting 
+    cookiesPurchased: function() {  // function for getting avg. cookies/sale
         this.randoNumber() 
         for (let i =0; i < hours.length; i++) {
         this.cookieSoldArray.push(Math.ceil(this.randoNumber() * this.avgCookieSale))   
         }
     },
-        cookieSoldArray: [],
+        cookieSoldArray: [], // function to insert onto HTML page
         render: function() {
             this.cookiesPurchased()
             for (let i=0; i < this.cookieSoldArray.length; i++) {
                 let li = document.createElement("li");
                 li.innerText=this.cookieSoldArray[i];
                 seattleList.appendChild(li);
-            }
+            }  //need store at top and tiems listed on side
         }
 }
 Seattle.render() 
+
+
+let Tokyo = {
+    storeName: 'Tokyo',   // Object properties
+    minCust: 3,            // Object properties
+    maxCust: 24,            // Object properties
+    avgCookieSale: 1.2,     // Object properties
+    randoNumber: function () {  // object function for random number generator
+        return Math.floor(Math.random() * (this.maxCust - this.minCust +1)) + this.minCust;
+    },
+    cookiesPurchased: function() {  
+        this.randoNumber() 
+        for (let i =0; i < hours.length; i++) {
+        this.cookieSoldArray.push(Math.ceil(this.randoNumber() * this.avgCookieSale))   
+        }
+    },
+        cookieSoldArray: [],  
+        render: function() {
+            this.cookiesPurchased()
+            for (let i=0; i < this.cookieSoldArray.length; i++) {
+                let li = document.createElement("li");
+                li.innerText=this.cookieSoldArray[i];
+                tokyoList.appendChild(li);
+            }
+        }
+}
+Tokyo.render() 
+
+let Dubai = {
+    storeName: 'Dubai',   // Object properties
+    minCust: 11,            // Object properties
+    maxCust: 38,            // Object properties
+    avgCookieSale: 3.7,     // Object properties
+    randoNumber: function () {  // object function for random number generator
+        return Math.floor(Math.random() * (this.maxCust - this.minCust +1)) + this.minCust;
+    },
+    cookiesPurchased: function() {  
+        this.randoNumber() 
+        for (let i =0; i < hours.length; i++) {
+        this.cookieSoldArray.push(Math.ceil(this.randoNumber() * this.avgCookieSale))   
+        }
+    },
+        cookieSoldArray: [],  
+        render: function() {
+            this.cookiesPurchased()
+            for (let i=0; i < this.cookieSoldArray.length; i++) {
+                let li = document.createElement("li");
+                li.innerText=this.cookieSoldArray[i];
+                dubaiList.appendChild(li);
+            }
+        }
+}
+Dubai.render() 
+
+let Paris = {
+    storeName: 'Paris',   // Object properties
+    minCust: 20,            // Object properties
+    maxCust: 38,            // Object properties
+    avgCookieSale: 2.3,     // Object properties
+    randoNumber: function () {  // object function for random number generator
+        return Math.floor(Math.random() * (this.maxCust - this.minCust +1)) + this.minCust;
+    },
+    cookiesPurchased: function() {  
+        this.randoNumber() 
+        for (let i =0; i < hours.length; i++) {
+        this.cookieSoldArray.push(Math.ceil(this.randoNumber() * this.avgCookieSale))   
+        }
+    },
+        cookieSoldArray: [],  
+        render: function() {
+            this.cookiesPurchased()
+            for (let i=0; i < this.cookieSoldArray.length; i++) {
+                let li = document.createElement("li");
+                li.innerText=this.cookieSoldArray[i];
+                parisList.appendChild(li);
+            }
+        }
+}
+Paris.render() 
+
+let Lima = {
+    storeName: 'Lima',   // Object properties
+    minCust: 2,            // Object properties
+    maxCust: 16,            // Object properties
+    avgCookieSale: 4.6,     // Object properties
+    randoNumber: function () {  // object function for random number generator
+        return Math.floor(Math.random() * (this.maxCust - this.minCust +1)) + this.minCust;
+    },
+    cookiesPurchased: function() {  
+        this.randoNumber() 
+        for (let i =0; i < hours.length; i++) {
+        this.cookieSoldArray.push(Math.ceil(this.randoNumber() * this.avgCookieSale))   
+        }
+    },
+        cookieSoldArray: [],  
+        render: function() {
+            this.cookiesPurchased()
+            for (let i=0; i < this.cookieSoldArray.length; i++) {
+                let li = document.createElement("li");
+                li.innerText=this.cookieSoldArray[i];
+                limaList.appendChild(li);
+            }
+        }
+}
+Lima.render() 
+
+
 
 
 
@@ -38,23 +149,6 @@ Seattle.render()
 // 4- Store results for each location in separate array... Perhaps as a property of the object representing the location.
 // 5- Display values of each array as unordered list
 // 6- Calculate sum of these hourly totals. Output should like below: & display the lists on sales.html.
-// 
-// Seattle 
-// 6am: 16 cookies
-// 7am: 20 cookies
-// 8am: 35 cookies
-// 9am: 48 cookies
-// 10am: 56 cookies
-// 11am: 77 cookies
-// 12am: 93 cookies
-// 1pm: 144 cookies
-// 2pm: 119 cookies
-// 3pm: 84 cookies
-// 4pm: 61 cookies
-// 5pm: 23 cookies
-// 6pm: 42 cookies
-// 7pm: 57 cookies
-// total: 875 cookies
 
 // Location    Min/Cust.   Max/Cust.   Avg. Cookie/sale
 // seattle         23          65    42      6.3  
